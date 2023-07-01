ccab8a7d9010a81c95ca0d517840f9945a815d44
Savannah Ostrowski <savannahostrowski@gmail.com>
2023-07-01 21:29:16 +0000

    work on if statements and testing

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Gossamer 🪶
 _used to refer to something very light, thin, or delicate_
 
-A Python interpreter, written in Go
+A (work in progress) Python interpreter, written in Go
 
 ## TODOs
 - [ ] Refactor variable statement to not use LET and ;
+- [ ] Remove support for single line if expressions
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -316,6 +316,106 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 	}
 }
 
+func TestIfStatement(t *testing.T) {
+	input := `
+if (x > y):
+	return x
+`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain %d statements. got=%d\n",
+			1, len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+    if !ok {
+        t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+            program.Statements[0])
+    }
+
+	exp, ok := stmt.Expression.(*ast.IfExpression)
+    if !ok {
+        t.Fatalf("stmt.Expression is not ast.IfExpression. got=%T",
+            stmt.Expression)
+    }
+
+    if !testInfixExpression(t, exp.Condition, "x", "<", "y") {
+        return
+    }
+
+	if len(exp.Consequence.Statements) != 1 {
+		t.Errorf("consequence is not 1 statements. got=%df\n", len(exp.Consequence.Statements))
+	}
+
+	consequence, ok := exp.Consequence.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("Statements[0] is not ast.ExpressionStatement. got=%T",
+		exp.Consequence.Statements[0])
+	}
+
+	if !testIdentifier(t, consequence.Expression, "x") {
+		return
+	}
+
+	if exp.Alternative != nil {
+		t.Errorf("exp.Alternative.Statements was not nil. got=%+v", exp.Alternative)
+	}
+}
+func TestIfSElseStatement(t *testing.T) {
+	input := `
+if (x > y):
+	return x
+else:
+	return y
+`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain %d statements. got=%d\n",
+			1, len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+    if !ok {
+        t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+            program.Statements[0])
+    }
+
+	exp, ok := stmt.Expression.(*ast.IfExpression)
+    if !ok {
+        t.Fatalf("stmt.Expression is not ast.IfExpression. got=%T",
+            stmt.Expression)
+    }
+
+    if !testInfixExpression(t, exp.Condition, "x", "<", "y") {
+        return
+    }
+
+	if len(exp.Consequence.Statements) != 1 {
+		t.Errorf("consequence is not 1 statements. got=%df\n", len(exp.Consequence.Statements))
+	}
+
+	consequence, ok := exp.Consequence.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("Statements[0] is not ast.ExpressionStatement. got=%T",
+		exp.Consequence.Statements[0])
+	}
+
+	if !testIdentifier(t, consequence.Expression, "x") {
+		return
+	}
+
+	if exp.Alternative != nil {
+		t.Errorf("exp.Alternative.Statements was not nil. got=%+v", exp.Alternative)
+	}
+}
 func testVariableStatement(t *testing.T, s ast.Statement, name string) bool {
 	if s.TokenLiteral() != "let" {
 		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
